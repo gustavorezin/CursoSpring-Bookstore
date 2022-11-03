7f77228f3dc9bf89a9e8ec125463b373d86166fa
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2022-11-03 09:25:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# CursoAngular-Bookstore
+-git init
+-git add README.md
+-git commit -m "first commit"
+-git branch -M main
+-git remote add origin https://github.com/gustavorezin/CursoAngular-Bookstore.git
+-git push -u origin main
---
+++ bookstore/src/test/java/com/gusta/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusta.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
